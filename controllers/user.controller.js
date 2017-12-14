126a67ae45932fe237843e92886fc9bad754f5de
m.zubair <m.zubair@centricsource.com>
2017-12-14 19:45:58 +0500

    express validation with custom middleware

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const validator = require('../middlewares/validator');
 const router = express.Router();
 
 module.exports = (Sequelize, passport, appConfig) => {
@@ -11,11 +12,12 @@ module.exports = (Sequelize, passport, appConfig) => {
                 return next(err);
             }
             if (!user) {
-                res.sendJSON({}, info.message, 401);
+                return res.sendJSON({}, info.message, 401);
             }
             req.logIn(user, function (err) {
                 if (err) {
-                    res.sendJSON({error: err}, 'Exception', 500);
+                    return next(err);
+//                    res.sendJSON({error: err}, 'Exception', 500);
                 }
                 res.sendJSON(user, 'User logedin successfully')
             });
@@ -27,7 +29,6 @@ module.exports = (Sequelize, passport, appConfig) => {
         res.sendJSON({}, "User loged out");
     });
 
-
     router.post('/register', (req, res) => {
         console.log(req.body);
         res.send('post register');
@@ -37,5 +38,11 @@ module.exports = (Sequelize, passport, appConfig) => {
 //        res.send('user has lodged in ');
 //    });
 
+    router.get('/roles', (req, res) => {
+
+        var users = userModel.findAll();
+
+        res.send(users);
+    });
     return router;
 };
---
+++ middlewares/validator.js
@@ -0,0 +1,41 @@
+/**
+ * This middleware will add a validation layer between a request and a controller.
+ * NOTE: Ther routes must be same as the routes in the controller file
+ * @author Muhammad Zubair
+ */
+
+/**
+ * Use a custome middleware to send validation errors
+ */
+module.exports.validationResult = (req, res, next) => {
+    if (req.validationErrors()) {
+        res.sendJSON({errors: req.validationErrors()}, 'Validation Errors');
+    } else {
+        next();
+    }
+};
+
+module.exports.user = (req, res, next) => {
+    //swith trough the mataching route for validation
+    switch (req.originalUrl) {
+        case '/user/login':
+        {
+            req.check('username').exists().isEmail();
+            req.check('password').exists();
+            console.log('called', req.validationErrors());
+            break;
+        }
+    }
+    next();
+};
+
+function authChecker(req, res) {
+    if (!req.user) {
+        res.status(401);
+        res.sendJSON({}, 'User session was timed out', 401);
+    } else {
+        //authorization implementation goes here
+        return true;
+    }
+}
+
