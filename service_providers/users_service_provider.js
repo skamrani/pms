3236d240f2c8599eb46f08dbece3b29c6cfceb54
atifibrahimcs <atif.ibrahim.cs@gmail.com>
2017-12-15 20:24:33 +0500

    service added for user now use it instead of using model directly

---
+++ service_providers/users_service_provider.js
@@ -0,0 +1,20 @@
+/* jshint indent: 2 */
+const Sequelize = require('../config/sequelize');
+var userModel = Sequelize.loadModel('users');
+
+userModel.getAllTeams = function () {
+
+
+    return userModel.rawAttributes.team.values;
+    /*
+    Sequelize.query("SHOW COLUMNS FROM users LIKE 'team'", { type: Sequelize.QueryTypes.SELECT }).then(values => {
+        return response;
+    })
+    */
+
+}
+
+userModel.xyz = function () {
+    console.log('xyz');
+}
+module.exports = userModel
